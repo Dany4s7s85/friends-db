c4a29f93751ad523cdbbc84fbe9107e9e276ad43
Dany4s7s85 <79710542+Dany4s7s85@users.noreply.github.com>
2022-05-31 05:19:30 -0700

    ertyuk

--- controlers/friendControler.js
+++ controlers/friendControler.js
@@ -1,47 +1,91 @@
 const Friend = require('../models/friendModel')
 
 exports.createFrient = async (req, res) =>{
-
+try {
     let doc = await Friend.create(req.body);
     res.status(200).json({
         status: "Success",
         data: { friend: doc, message: "Firendcreated successfully" },
       });
+}catch(err){
+    
+    res.status(400).json({
+        status: "Error",
+        data: { message: err.message },
+      });
+}
+ 
 
 }
 
 exports.getAllFriend =async (req, res)=>{
-
+   try{
     let doc = await Friend.find();
     res.status(200).json({
         status: "Success",
         data: { friend: doc, message: "Got one friend" },
       });
+   }catch(err){
+    
+    res.status(400).json({
+        status: "Error",
+        data: { message: err.message },
+      });
+}
+    
 }
 
 exports.getOneFriend = async(req, res) =>{
-    let doc = await Friend.findById(req.params.id)
+
+    try{
+        let doc = await Friend.findById(req.params.id)
     res.status(200).json({
         status: "Success",
         data: { friend: doc, message: "Firendcreated successfully" },
       });
+    }catch(err){
+    
+        res.status(400).json({
+            status: "Error",
+            data: { message: err.message },
+          });
+    }
+    
 }
 
 
 exports.getOneFriendAndUpdate = async(req, res) =>{
-    let doc = await Friend.findByIdAndUpdate(req.params.id,req.body,{
-        new:true
-    })
-    res.status(200).json({
-        status: "Success",
-        data: { friend: doc, message: "Firendcreated successfully" },
-      });
+    try{
+        let doc = await Friend.findByIdAndUpdate(req.params.id,req.body,{
+            new:true
+        })
+        res.status(200).json({
+            status: "Success",
+            data: { friend: doc, message: "Firendcreated successfully" },
+          });
+    }catch(err){
+    
+        res.status(400).json({
+            status: "Error",
+            data: { message: err.message },
+          });
+    }
+    
 }
 
 exports.getOneFriendAndDelete = async(req, res) =>{
-    let doc = await Friend.findByIdAndDelete(req.params.id)
+    try{
+        let doc = await Friend.findByIdAndDelete(req.params.id)
     res.status(200).json({
         status: "Success",
         data: { friend: doc, message: "Firendcreated successfully" },
       });
+    }catch(err){
+    
+        res.status(400).json({
+            status: "Error",
+            data: { message: err.message },
+          });
+    }
+    
 }
